26ca626a576abe3073cf0d19ec24fb332b1150bc
Meenakshi Singh <meenakshisingh0722@gmail.com>
2026-06-15 08:39:18 +0000

    updated file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId =123
+let accountEmail ="phugga@google.com"
+var accountPassword = "1234"
+accountCity ="Jaipur"
+let accountState;
+
+//accountId =2// not allowed
+console.log(accountId);
+
+accountEmail="phuggi@gmail.com"
+accountPassword="212121"
+accountCity="Delhi"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
