511593c362692d9fd7981a6fd6d3005d3f867302
Meenakshi Singh <meenakshisingh0722@gmail.com>
2026-06-15 11:31:56 +0000

    stack and heap

--- 01_basics/dataTypesSummary.js
+++ 01_basics/dataTypesSummary.js
@@ -28,3 +28,22 @@ const myFunction = function(){
 }
 
 console.log(typeof anotherId);
+
+//stack: primitive , var ki copy 
+//heap :non primitive , change in original value
+
+let ytChannel='phugga'
+let anotherChannel=ytChannel
+anotherChannel='meenakshi' //change in copy
+console.log(ytChannel); 
+console.log(anotherChannel); 
+
+let user1={
+    email:"user@gmail.com",
+    upi:"user@ybl"
+}
+let user2=user1
+user2.email="meenakshi.com" //original changes
+
+console.log(user1.email);
+console.log(user2.email);
