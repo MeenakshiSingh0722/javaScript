edf57b0b9f2e79717f8dc744ee1ac2c24c7432b1
Meenakshi Singh <meenakshisingh0722@gmail.com>
2026-06-15 09:41:39 +0000

    conversions and operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,62 @@
+let score='33'
+
+//33abc=>NAN 
+//null=>0
+//undefined=>NAN 
+//true=>1
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+//console.log(booleanIsLoggedIn)
+
+let someNumber=33
+
+
+let stringNumber=String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value=3
+let negValue = -value
+//console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3); //divide
+// console.log(2%3); //remainder
+
+let str1="hello"
+let str2=" phugga"
+
+let str3= str1+str2
+//console.log(str3);
+
+// console.log("1" + 2); //12
+// console.log(1 + "2"); //12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2"); //32
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true); //1
+//// console.log(true+); //error
+// console.log(+""); //0
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
