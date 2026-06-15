5358d582ebda8d00c13ddf0a8644a6265cd10007
Meenakshi Singh <meenakshisingh0722@gmail.com>
2026-06-15 10:23:41 +0000

    types of data types

---
+++ 01_basics/dataTypesSummary.js
@@ -0,0 +1,30 @@
+//primitive:call by value
+//7: string, number, boolean, null, undefined, symbol, bigInt
+
+const score = 100
+const scoreValue=100.3
+const isLoggedIn = false
+const outsideTemp=null
+let userEmail;
+
+const id = Symbol('123')
+const anotherid= Symbol('123')
+console.log(id===anotherid);
+
+// const bigNumber = 3456543576654356754n
+
+//reference(non primitive):call by reference
+//array, objects , functions 
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {
+    name: "phugga",
+    age: 21,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
