8428f56984ed1737692767ef542fabea0b41a745
Meenakshi Singh <meenakshisingh0722@gmail.com>
2026-06-17 11:08:56 +0000

    date & time

--- 01_basics/07_datesInJS.js
+++ 01_basics/07_datesInJS.js
@@ -8,24 +8,23 @@ let date = new Date()
 
 // let createDate = new Date(2026, 0, 23)
 // let createDate = new Date(2026, 0, 23, 5,6)
-// let createDate = new Date("2023-01-14")
-let createDate = new Date("01-14-2023")
+// let createDate = new Date("2023-01-14") //yyyy-mm-dd
+let createDate = new Date("01-14-2023") //mm-yyyy-dd
 // console.log(createDate.toLocaleString());
 
 let myTimeStamp = Date.now()
 
-// console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+// console.log(myTimeStamp); //ms
+// console.log(myCreatedDate.getTime()); //ms
+// console.log(Math.floor(Date.now()/1000)); //in sec
 
 let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1); //cause in js month starts with 0
+// console.log(newDate.getDay());
 
-// `${newDate.getDay()} and the time `
+// `${newDate.getDay()} and the time `${newDate.getTime()}`
 
 newDate.toLocaleString('default', {
     weekday: "long",
-    
 })
