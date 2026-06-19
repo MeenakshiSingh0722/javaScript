93c1d832f2239c74ab1d0193269ada519a846e71
Meenakshi Singh <meenakshisingh0722@gmail.com>
2026-06-19 18:18:03 +0000

    for loop

--- 05_iteration/01_forLoop.js
+++ 05_iteration/01_forLoop.js
@@ -12,15 +12,15 @@
 //    for (let j = 1; j <= 10; j++) {
     //console.log(`Inner loop value ${j} and inner loop ${i}`);
     // console.log(i + '*' + j + ' = ' + i*j );
-   }
-}
+   //}
+//}
 
 // let myArray = ["flash", "batman", "superman"]
 //console.log(myArray.length);
 // for (let index = 0; index < myArray.length; index++) {
 //     const element = myArray[index];
     //console.log(element);
-}
+//}
 //if array ke bhr value chli jati hai then print undefined 
 
 // break and continue
