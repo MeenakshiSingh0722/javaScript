847a80e22c4722cec25795308f7553b407f4410e
Meenakshi Singh <meenakshisingh0722@gmail.com>
2026-06-19 17:20:16 +0000

    if else statement

---
+++ 04_control_flow/one.js
@@ -0,0 +1,47 @@
+//if stt
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const temp =41
+
+// if(temp<50){
+//     console.log("less than 50");
+// }else{
+//     console.log("temperature is greater than 50");
+    
+// }
+// console.log("temperature is greater than 50");
+
+// const score = 200
+// if (score > 100) {
+//     let power = "fly"  //if var use then can use outside the scope
+//     console.log(`User power: ${power}`); 
+// }
+// console.log(`User power: ${power}`);
+
+// const balance =1000
+// // if (balance>500) console.log("test");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 900");
+    
+// } else {
+//     console.log("less than 1200");  
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
